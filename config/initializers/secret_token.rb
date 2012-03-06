0c33fe6240a7b608406562ed3ebf09f0b38daf07
Sam Priestley <sfpriestley@hotmail.co.uk>
2012-03-06 15:59:24 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etf::Application.config.secret_token = 'e6af2906996f703c199373e65f60d02bfd3f77e596709e727f8d37aac5d5b6dd35e7ebe6c4e1c444ea2178aad2fb4a4573f3314938d41683e849212007c20cb1'
